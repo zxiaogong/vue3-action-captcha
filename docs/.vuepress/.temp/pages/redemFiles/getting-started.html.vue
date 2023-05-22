c07b6f5363d621eedb1dbdbec0bd28c39e649a4b
zxiaogong <1120197042@qq.com>
2023-05-23 02:08:51 +0800

    package:更新依赖

--- docs/.vuepress/.temp/pages/redemFiles/getting-started.html.vue
+++ docs/.vuepress/.temp/pages/redemFiles/getting-started.html.vue
@@ -1,18 +1,17 @@
 <template><div><h1 id="快速上手" tabindex="-1"><a class="header-anchor" href="#快速上手" aria-hidden="true">#</a> 快速上手</h1>
-<h3 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h3>
+<h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h2>
 <p>yarn add vue3-action-captcha</p>
-<h3 id="全局引入" tabindex="-1"><a class="header-anchor" href="#全局引入" aria-hidden="true">#</a> 全局引入</h3>
-<div class="language-typescript line-numbers-mode" data-ext="ts"><pre v-pre class="language-typescript"><code><span class="token keyword">import</span> <span class="token punctuation">{</span> createApp <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">'vue'</span>
-<span class="token keyword">import</span> App <span class="token keyword">from</span> <span class="token string">'./App.vue'</span>
-<span class="token keyword">import</span> ActionCaptcha <span class="token keyword">from</span> <span class="token string">"vue3-action-captcha"</span>
+<h2 id="main-js中-全局注册组件" tabindex="-1"><a class="header-anchor" href="#main-js中-全局注册组件" aria-hidden="true">#</a> main.js中 全局注册组件</h2>
+<div class="language-typescript line-numbers-mode" data-ext="ts"><pre v-pre class="language-typescript"><code><span class="token keyword">import</span> ActionCaptcha <span class="token keyword">from</span> <span class="token string">"vue3-action-captcha"</span>
 <span class="token keyword">import</span> <span class="token string">"vue3-action-captcha/dist/lib/style.css"</span>
 <span class="token function">createApp</span><span class="token punctuation">(</span>App<span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">use</span><span class="token punctuation">(</span>ActionCaptcha<span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">mount</span><span class="token punctuation">(</span><span class="token string">'#app'</span><span class="token punctuation">)</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="局部引入" tabindex="-1"><a class="header-anchor" href="#局部引入" aria-hidden="true">#</a> 局部引入</h3>
-<div class="language-typescript line-numbers-mode" data-ext="ts"><pre v-pre class="language-typescript"><code><span class="token keyword">import</span> <span class="token punctuation">{</span> createApp <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">'vue'</span>
-<span class="token keyword">import</span> App <span class="token keyword">from</span> <span class="token string">'./App.vue'</span>
-<span class="token keyword">import</span> <span class="token punctuation">{</span> SliderCaptcha <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue3-action-captcha"</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-typescript line-numbers-mode" data-ext="ts"><pre v-pre class="language-typescript"><code><span class="token keyword">import</span> <span class="token punctuation">{</span> SliderCaptcha <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue3-action-captcha"</span>
 <span class="token keyword">import</span> <span class="token string">"vue3-action-captcha/dist/lib/style.css"</span>
 <span class="token function">createApp</span><span class="token punctuation">(</span>App<span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">component</span><span class="token punctuation">(</span><span class="token string">"Slider-captcha"</span><span class="token punctuation">,</span> SliderCaptcha<span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">mount</span><span class="token punctuation">(</span><span class="token string">'#app'</span><span class="token punctuation">)</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></div></template>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="在组件中按需引入" tabindex="-1"><a class="header-anchor" href="#在组件中按需引入" aria-hidden="true">#</a> 在组件中按需引入</h2>
+<div class="language-typescript line-numbers-mode" data-ext="ts"><pre v-pre class="language-typescript"><code><span class="token comment">// 注意：按需引入时也需要在main.js中引入样式文件 import "vue3-action-captcha/dist/lib/style.css"</span>
+<span class="token keyword">import</span> <span class="token punctuation">{</span> DragCaptcha <span class="token punctuation">,</span>SliderCaptcha<span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue3-action-captcha"</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></div></template>
 
 
